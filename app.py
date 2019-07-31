95b6d7a68b4a353157b575b7271a7ba525cbaa69
myname21-meet <myname21@meet.mit.edu>
2019-07-31 07:06:41 -0400

    fix

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home_page():
+	return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+   app.run(debug = True)
